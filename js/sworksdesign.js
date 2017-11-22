4a22e50c54a94ef5fbd14da1e03efef27944497c
Gordon Wong <gwong001@gmail.com>
2017-11-21 21:54:55 -0800

    Remove Parse and contact form

--- js/sworksdesign.js
+++ js/sworksdesign.js
@@ -1,9 +1,6 @@
 var SCROLL_SPEED = 1000;
 var IMAGE_FADE_DELAY = 600;
 var MATERIALBOX_ZOOM_DURATION = 200;
-var PARSE_API_URL = 'https://api.parse.com/1/functions/email';
-var PARSE_APP_ID = 'cOLehws9n6R0OlPU6sN5yNe2bNrd0eiYct8YLHdi';
-var PARSE_REST_API_KEY = '6akvWrdhzRJ4RQv7QmK6SjhT43sGCY5Mql4GcOI5';
 var YOUTUBE_IFRAME_API = 'https://www.youtube.com/iframe_api';
 var youTubePlayer;
 var isYoutubeAPIReady;
@@ -163,89 +160,6 @@ function hideVideoLoadingSpinner() {
   loadingSpinner.removeClass('active');
 }
 
-function flipContactFormCard(side) {
-  // Internet explorer doesn't support the CSS 3D flip transform
-  if (isIE()) {
-    switch (side) {
-      case 'front':
-        $('#contact-form-message-container').css('transform', 'rotateY(180deg)');
-        $('#contact-form-message-container').css('z-index', '0');
-        break;
-      case 'back':
-        $('#contact-form-message-container').css('transform', 'none');
-        $('#contact-form-message-container').css('z-index', '1');
-        break;
-    }
-    $('#contact-form-message-container').toggleClass('card-panel');
-  } else {
-    switch (side) {
-      case 'front':
-        $('#contact-form-flip-container > .card-panel').removeClass('flipped');
-        break;
-      case 'back':
-        $('#contact-form-flip-container > .card-panel').addClass('flipped');
-        break;
-    }
-  }
-}
-
-function resetContactForm() {
-  flipContactFormCard('front');
-  // Clear the form
-  $('#contact-form')[0].reset();
-  // Hide after a delay so that they are still shown during the animation
-  setTimeout(function() {
-    $('#contact-form-message-container .green-text').addClass('hide');
-    $('#contact-form-message-container .red-text').addClass('hide');
-  }, 1000);
-}
-
-function showContactSuccessMessage() {
-  $('#contact-form-message-container .green-text').removeClass('hide');
-  $('#contact-form-message-container .red-text').addClass('hide');
-}
-
-function showContactErrorMessage() {
-  $('#contact-form-message-container .green-text').addClass('hide');
-  $('#contact-form-message-container .red-text').removeClass('hide');
-}
-
-function submitContactForm() {
-  // Build headers
-  var headers = {
-    'x-parse-application-id': PARSE_APP_ID,
-    'x-parse-rest-api-key': PARSE_REST_API_KEY
-  };
-
-  // Build data
-  var data = {
-    name: $('#contact-form-name').val(),
-    email: $('#contact-form-email').val(),
-    subject: $('#contact-form-subject').val(),
-    message: $('#contact-form-message').val()
-  };
-
-  // Send email request
-  $.ajax({
-    method: 'POST',
-    url: PARSE_API_URL,
-    headers: headers,
-    data: data,
-    crossDomain: true,
-    dataType: 'json'
-  }).done(function(data) {
-    showContactSuccessMessage();
-  }).fail(function(error) {
-    showContactErrorMessage();
-    // Flip card back after a delay
-    setTimeout(function() {
-      resetContactForm();
-    }, 3000);
-  }).always(function() {
-    flipContactFormCard('back');
-  });
-}
-
 function isIE() {
   var msie = window.navigator.userAgent.indexOf("MSIE ");
   return (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./));
@@ -261,10 +175,4 @@ $(function() {
 
   // Initialize parallax images
   $('.parallax').parallax();
-
-  // Handle contact form submission
-  $('#contact-form').on('submit', function(e) {
-    e.preventDefault();
-    submitContactForm();
-  });
 });
